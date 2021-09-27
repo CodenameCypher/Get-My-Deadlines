a0ce257b4ac92c1a8a6b7be94bf65eb656ded005
Aditya Roy <54760764+CodenameCypher@users.noreply.github.com>
2021-09-27 15:08:29 +0600

    new templates and forms

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from main.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', homepage, name='home'),
+    path('deadlines/', get_deadlines, name='deadlines'),
+    path('contact/', contact, name='contact'),
+    path('about_me/', about, name='about'),
+    path('faq/', faq, name='faq'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/views.py
+++ main/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def homepage(request):
+    if request.method == 'POST':
+        buX_email = request.POST.get('email')
+        buX_password = request.POST.get('password')
+        semester = request.POST.get('semester')
+        print(semester)
+        return render(request, 'results.html')
+    return render(request, 'index.html')
+
+
+def get_deadlines(request):
+    if request.method == 'POST':
+        print(request.POST.get('email'))
+        return render(request, 'results.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def faq(request):
+    return render(request, 'faq.html')
