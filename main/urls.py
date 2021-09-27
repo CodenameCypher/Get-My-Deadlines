a436266e3b0199bb3998f2f339e337dec0a1599b
Aditya Roy <54760764+CodenameCypher@users.noreply.github.com>
2021-09-27 21:27:36 +0600

    final release v3

--- main/urls.py
+++ main/urls.py
@@ -7,8 +7,6 @@ from main.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homepage, name='home'),
-    path('deadlines/', get_deadlines, name='deadlines'),
-    path('contact/', contact, name='contact'),
     path('about/', about, name='about'),
     path('faq/', faq, name='faq'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
